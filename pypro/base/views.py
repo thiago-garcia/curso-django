570ac4dd7ddc2bb4c6bde8b511d73f5e27869b16
Thiago Garcia <thiago-garcia@outlook.com>
2024-04-01 17:16:41 -0300

    adicionado os arquivos estaticos do bootstrap, criado template e ajustado views close #54

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render # noqa
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('<html><body>Olá Django</body></html>', content_type='text/html; charset=utf-8')
+    return render(request, 'base/home.html')
