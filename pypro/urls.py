98ecdf3970597dd5523634daa93fca7947eb8908
Thiago Garcia <thiago-garcia@outlook.com>
2024-04-03 00:38:52 -0300

    refatorado url da app base #54

--- pypro/urls.py
+++ pypro/urls.py
@@ -17,11 +17,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
-from pypro.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path('', include('pypro.base.urls')),
 ]
 
 
