08210d3d0f26cf6a67504fe340d380b8501ffea5
Thiago Garcia <thiago-garcia@outlook.com>
2024-04-03 00:16:27 -0300

    incluido url nomeada, reverse url nos testes e template tag url #54

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,13 +1,18 @@
-from django.test import Client
+from django.urls import reverse
+import pytest
 
 from pypro.django_assertions import assert_contains
 
 
-def test_status_code(client: Client):
-    resp = client.get('/')
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('home'))
+    return resp
+
+
+def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_title(client: Client):
-    resp = client.get('/')
+def test_title(resp):
     assert_contains(resp, '<title>Python Pro</title>')
