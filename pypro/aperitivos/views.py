4751413039ab0f3d0487ec39a4ca082195a76793
Thiago Garcia <thiago-garcia@outlook.com>
2024-04-06 22:31:42 -0300

    criado template base, atualizado templates para usar herança de base e adicionado variaveis através de contexto close #64

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 288344114}
+
+    return render(request, 'aperitivos/video.html', context={'video': video})
