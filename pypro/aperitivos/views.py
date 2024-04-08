f63784c85fc52dd34ec750f5c2e519a44a0b9129
Thiago Garcia <thiago-garcia@outlook.com>
2024-04-08 16:47:19 -0300

    adicionado segundo video close #66

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 288344114}
-
+    videos = {
+        'motivacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 288344114},
+        'instalacao-windows': {'titulo': 'Instalação Windows', 'vimeo_id': 876176372},
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
