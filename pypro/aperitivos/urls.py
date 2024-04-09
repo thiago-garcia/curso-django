8f6b4e5935b991a6e2973b5023164ecd444d7372
Thiago Garcia <thiago-garcia@outlook.com>
2024-04-08 23:12:37 -0300

    criado a página indice e tests close #70

--- pypro/aperitivos/urls.py
+++ pypro/aperitivos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from pypro.aperitivos.views import video
+from pypro.aperitivos.views import indice, video
 
 app_name = 'aperitivos'
 urlpatterns = [
     path('<slug:slug>', video, name='video'),
+    path('', indice, name='indice'),
 ]
